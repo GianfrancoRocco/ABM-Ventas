df5f3365a03fe1355b20716540868821ec879303
GianfrancoRocco <gianfrancorocco.business@gmail.com>
2020-08-23 17:07:45 -0300

    Corrección ortográfica

--- modal-logout.php
+++ modal-logout.php
@@ -2,7 +2,7 @@
     <div class="modal-dialog" role="document">
       <div class="modal-content">
         <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">¿Estas seguro?</h5>
+          <h5 class="modal-title" id="exampleModalLabel">¿Estás seguro?</h5>
           <button class="close" type="button" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">×</span>
           </button>
@@ -10,7 +10,7 @@
         <div class="modal-body">Haga click en "Cerrar sesión" si desea finalizar la sesión actual.</div>
         <div class="modal-footer">
           <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancelar</button>
-          <a class="btn btn-primary" href="login.php">Cerrar sesion</a>
+          <a class="btn btn-primary" href="login.php">Cerrar sesión</a>
         </div>
       </div>
     </div>
